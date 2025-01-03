04e403b63dcbb460ab937cd5bbc2b0ef47098ff3
derg203 <ddergacev714@gmail.com>
2025-01-03 16:56:13 +0300

    Update module_6_2_.py

--- module_6_2_.py
+++ module_6_2_.py
@@ -3,62 +3,42 @@ class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
-
     def __init__(self, speed):
-        self._cords = [0, 0, 0]  # Координаты в пространстве
+        self._cords = [0, 0, 0]
         self.speed = speed
-
     def move(self, dx, dy, dz):
-        new_cords_z = self._cords[2] + dz * self.speed
-        if new_cords_z > 0 :
-            self._cords[0] += dx * self.speed
-            self._cords[1] += dy * self.speed
-            self._cords[2] = new_cords_z
-        else: print("It's too deep, i can't dive :(" )
-
-    def get_cords(self):
-        print(f'X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
-
-    def attack(self):
-        if self._DEGREE_OF_DANGER < 5 :
-            print("Sorry, i'm peaceful :)")
-        elif self._DEGREE_OF_DANGER >= 5:
-            print("Be careful, i'm attacking you 0_0")
-
-class Bird (Animal):
-
-    def __init__(self, speed):
-        super().__init__(speed)
-        self.beak = True
-
-    def lay_eggs(self):
-        print(f"Here are(is) {random.randint(1,5)} eggs for you")
-
-class AquaticAnimal(Animal):
-    _DEGREE_OF_DANGER = 3
-
-    def dive_in(self, dz):
-        dz_abs= abs(dz)
-        new_z= self._cords[2] - (dz_abs / 2) * self.speed
+        new_x = self._cords[0] + dx * self.speed
+        new_y = self._cords[1] + dy * self.speed
+        new_z = self._cords[2] + dz * self.speed
         if new_z < 0:
             print("It's too deep, i can't dive :(")
         else:
-            self._cords[2] = new_z
-
+            self._cords = [new_x, new_y, new_z]
+    def get_cords(self):
+        print(f'X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
+class Bird(Animal):
+    beak = True
+    def lay_eggs(self):
+        num_of_eggs = random.randint(1, 4)
+        print(f"Here are(is) {num_of_eggs} eggs for you")
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+    def dive_in(self, dz):
+        new_z = self._cords[2] - abs(dz) * .5 * self.speed
+        self._cords[2] = max(new_z, 0)
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
-
-class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
-
+class Duckbill(PoisonousAnimal, Bird, AquaticAnimal):
+    sound = "Click-click-click"
     def __init__(self, speed):
         super().__init__(speed)
-        self.sound = "Click-click-click"  # Звук утконоса
-        # Устанавливаем степень опасности для атаки
-        self._DEGREE_OF_DANGER = 8
-
     def speak(self):
-        return print(self.sound)
-
+        print(self.sound)
 db = Duckbill(10)
 print(db.live)
 print(db.beak)
